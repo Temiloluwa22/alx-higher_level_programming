94cc122631d4bea0489178ca3c2e90629bb6b5b7
Temiloluwa22 <113702411+Temiloluwa22@users.noreply.github.com>
2023-09-28 23:53:32 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,22 +3,25 @@
 
 
 def find_peak(list_of_integers):
-    """Find a peak in the list_of_intergers"""
+    """
+    Find a peak in a list of unsorted integers using a binary search-like approach.
+    
+    Args:
+    list_of_integers (list): List of unsorted integers.
 
-    if list_of_integers is None or list_of_integers == [] 
-        return None
-    lo  = 0
-    hi  = len(list_of_integers)
-    mid = ((hi -lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1: 
-        return list_of_integers[0]
-    if hi == 2: 
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\ 
-            list_of_intergers[mid] >= list_of_integers[mid + 1]: 
-        return list_of_intergers[mid]
-    if mid > 0 and list_of_integers[mid]  < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers(mid:])
-    if mid > 0 and list_of_integers[mid]  < list_of_integers[mid - 1]:
-                return find_peak(list_of_integers[:mid])
+    Returns:
+    int: The peak element found in the list.
+    """
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        # Compare the middle element with its neighbors
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    # At the end of the loop, 'left' points to a peak element
+    return list_of_integers[left]
