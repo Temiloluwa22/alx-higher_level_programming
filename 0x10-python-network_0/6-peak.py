7c891a7d18e31ef69faf19ad01ac9ad1e085e05e
Temiloluwa22 <113702411+Temiloluwa22@users.noreply.github.com>
2023-09-28 23:44:07 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -18,7 +18,7 @@ def find_peak(list_of_integers):
     if list_of_integers[mid] >= list_of_integers[mid - 1] and\ 
             list_of_intergers[mid] >= list_of_integers[mid + 1]: 
         return list_of_intergers[mid]
-    if mid > 1 and list_of_integers[mid]  < list_of_integers[mid + 1]:
+    if mid > 0 and list_of_integers[mid]  < list_of_integers[mid + 1]:
         return find_peak(list_of_integers(mid:])
-    if mid > 1 and list_of_integers[mid]  < list_of_integers[mid - 1]:
+    if mid > 0 and list_of_integers[mid]  < list_of_integers[mid - 1]:
                 return find_peak(list_of_integers(mid])
