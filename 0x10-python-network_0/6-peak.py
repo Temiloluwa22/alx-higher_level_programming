e7ec0e0be47d429259b04879d3938e0a13cf7d2d
Temiloluwa22 <113702411+Temiloluwa22@users.noreply.github.com>
2023-09-29 02:39:40 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,14 +3,21 @@
 
 
 def find_peak(list_of_integers):
-    """A function that finds a peak in a list of unsorted integers"""
-    prev = 0
-    for idx, num in enumerate(list_of_integers):
-        if idx:
-            prev = list_of_integers[idx - 1]
-        if idx < len(list_of_integers) - 1:
-            next = list_of_integers[idx + 1]
-        else:
-            next = 0
-        if num >= prev and num >= next:
-            return num
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
